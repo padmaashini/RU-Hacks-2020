938d7eaff81615adeba632a132107d40bca598a4
Jin Kim <jink8036@gmail.com>
2020-05-16 17:33:03 -0700

    Update Session.py

--- RUHACKS-2020-Project/Scripts/Session.py
+++ RUHACKS-2020-Project/Scripts/Session.py
@@ -50,17 +50,6 @@ class Session:
     def addNewWebsite(self, website):
         #stub
 
-    def popupmsgtest(msg):
-
-        NORM_FONT = ("Helvetica", 10)
-
-        popup = tk.Tk()
-        popup.wm_title("!")
-        label = ttk.Label(popup, text=msg, font=NORM_FONT)
-        label.pack(side="top", fill="x", pady=10)
-        B1 = ttk.Button(popup, text="Okay", command=popup.destroy)
-        B1.pack()
-        popup.mainloop()
 
 
 
