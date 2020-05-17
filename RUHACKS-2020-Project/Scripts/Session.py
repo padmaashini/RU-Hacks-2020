ad9851ce0c7434d3845d70c57900ab71f8b173c7
Jin Kim <jink8036@gmail.com>
2020-05-16 17:25:48 -0700

    Initial Commit
    Most of the session backend is written. Just need to figure out how to create CSV's.

---
+++ RUHACKS-2020-Project/Scripts/Session.py
@@ -0,0 +1,80 @@
+import datetime
+import csv
+import os.path
+import tkinter as tk
+from tkinter import ttk
+
+class Session:
+    def __init__(self, name, profileName):
+
+        self.name = name
+        self.timeStarted = datetime.datetime.now()
+        self.timeEnded = None
+
+        self.beingProductive = False
+        self.secondsProductive = 0
+        self.secondsLazy = 0
+
+        #A DICTIONARY variable that receives the website and whether it is productive or not
+        #Key - website, value - productive/unproductive (boolean, T/F)
+        self.newWebsites = {}
+
+        # The name of the profile associated with this session
+        self.profileName = profileName
+
+    def end(self):
+        self.timeEnded = datetime.datetime.now()
+        self.save()
+
+    def checkProductivity(self, isProductive):
+        # called with whether current active tab is productive or not
+        # called every time active tab switches
+        if isProductive:
+            self.beingProductive = True
+        else:
+            self.beingProductive = False
+
+    def tick(self):
+        # called every second
+        if self.beingProductive:
+            self.secondsProductive = self.secondsProductive + 1
+        else:
+            self.secondsLazy = self.secondsLazy + 1
+
+    def save(self):
+        file_to_open = os.path.join("RUHACKS-2020-PROJECT", "Profiles", (self.profileName + ".csv"))
+        with open(file_to_open, 'w') as csv_file:
+            csv_writer = csv.writer()
+            csv_writer.writerow(self.name, self.timeStarted, self.timeEnded, self.secondsProductive, self.secondsLazy, len(self.newWebsites))
+
+    def addNewWebsite(self, website):
+        #stub
+
+    def popupmsgtest(msg):
+
+        NORM_FONT = ("Helvetica", 10)
+
+        popup = tk.Tk()
+        popup.wm_title("!")
+        label = ttk.Label(popup, text=msg, font=NORM_FONT)
+        label.pack(side="top", fill="x", pady=10)
+        B1 = ttk.Button(popup, text="Okay", command=popup.destroy)
+        B1.pack()
+        popup.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
